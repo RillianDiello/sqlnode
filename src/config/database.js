30dfe233c5f20b9a659d8c3859bda664874b0366
Rillian Diello <rillian@favish.com>
2023-10-12 22:22:20 -0400

    Add dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 module.exports = {
-  dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "rootroot",
-  database: "sqlnode",
+  dialect: process.env.DIALECT,
+  host: process.env.HOST,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   define: {
     timestamps: true,
     underscored: true,
